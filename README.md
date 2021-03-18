4b4483b9983b5c66b4afd42004b038208c1b70d5
Paul Yeoh <paulyeoh@thoughtworks.com>
2021-03-18 09:32:59 +0800

    Added minor improvements after testing on pipeline

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-
+# MSSQL image with directory for seed files
 ## Based on https://github.com/twright-msft/mssql-node-docker-demo-app
 
-## Accept configuration variables
-https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-configure-environment-variables?view=sql-server-2017
-
 ## Required envvars
-
 - MSSQL_SA_PASSWORD
 
 ## Usage
 Put seed .sql files in ./myseedfiles, then mount as volume to docker-entrypoint-initdb.d
-
 
 ```
    docker build . -t local/mssql-seed
--- seed_data.sh
+++ seed_data.sh
@@ -3,15 +3,22 @@
 # do this in a loop because the timing for when the SQL instance is ready is indeterminate
 for i in {1..50};
 do
-  # wait for healthcheck to pass
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "SELECT 1"
+  echo "trying to health check db $i"
+  # wait for health check to pass
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "SELECT 1" -j
   if [ $? -eq 0 ]
   then
     for filename in /docker-entrypoint-initdb.d/*.sql; do
-      echo "XXXXXX RUNNING $filename XXXXXXXXXX"
-      /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i $filename
+      echo
+      echo "RUNNING $filename"
+      echo
+      /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -i "$filename" -b -m-1
     done
-    echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX INIT SCRIPTS COMPLETED XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    echo
+    echo
+    echo "INIT SCRIPTS COMPLETED"
+    echo
+    echo
     break
   else
     echo "not ready yet..."
